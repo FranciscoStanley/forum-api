149784b206c06ab8e0bdd3ac1e644205a3588685
Francisco Stanley <franciscothestanley@gmail.com>
2024-06-08 14:45:40 -0300

    feat: user implementation

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,83 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { Prisma, User as UserModel } from '@prisma/client';
+import { UserService } from './user.service';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private userService: UserService) {}
+
+  @Post()
+  async createUser(@Body() userData: Prisma.UserCreateInput): Promise<string> {
+    try {
+      const message = await this.userService.createUser(userData);
+      return message;
+    } catch (error) {
+      throw new HttpException(
+        'Erro ao criar usuário',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<UserModel> {
+    try {
+      const user = await this.userService.getUserById({ id: Number(id) });
+      if (!user) {
+        throw new HttpException('Usuário não encontrado', HttpStatus.NOT_FOUND);
+      }
+      return user;
+    } catch (error) {
+      throw new HttpException(
+        'Erro ao buscar usuário',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Put(':id')
+  async updateUserById(
+    @Body() userData: Prisma.UserUpdateInput,
+    @Param('id') id: string,
+  ): Promise<string> {
+    try {
+      const message = await this.userService.updateUserById(
+        Number(id),
+        userData,
+        {
+          where: { id: Number(id) },
+          data: userData,
+        },
+      );
+      return message;
+    } catch (error) {
+      throw new HttpException(
+        'Erro ao atualizar usuário',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  @Delete(':id')
+  async deleteUserById(@Param('id') id: string): Promise<string> {
+    try {
+      const message = await this.userService.deleteUserById({ id: Number(id) });
+      return message;
+    } catch (error) {
+      throw new HttpException(
+        'Erro ao excluir usuário',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,109 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Prisma, User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/database/prisma.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  @Inject()
+  private readonly prisma: PrismaService;
+
+  async getUserById(
+    userWhereUniqueInput: Prisma.UserWhereUniqueInput,
+  ): Promise<User | null> {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: userWhereUniqueInput,
+      });
+
+      if (!user) {
+        return null;
+      }
+
+      const { password, ...safeUser } = user;
+
+      return safeUser as User;
+    } catch (error) {
+      throw new Error(`Não foi possível buscar o usuário: ${error.message}`);
+    }
+  }
+
+  async createUser(data: Prisma.UserCreateInput): Promise<string> {
+    try {
+      const existingEmail = await this.prisma.user.findUnique({
+        where: {
+          email: data.email,
+        },
+      });
+
+      if (existingEmail) {
+        throw new Error('Este e-mail já está em uso.');
+      }
+
+      const saltOrRounds = 10;
+      const hashPassword = await bcrypt.hash(data.password, saltOrRounds);
+      await this.prisma.user.create({
+        data: {
+          ...data,
+          password: hashPassword,
+        },
+      });
+      return 'Usuário criado com sucesso.';
+    } catch (error) {
+      throw new Error(`Erro ao criar usuário: ${error.message}`);
+    }
+  }
+
+  async updateUserById(
+    id: number,
+    userData: Prisma.UserUpdateInput,
+    params: {
+      where: Prisma.UserWhereUniqueInput;
+      data: Prisma.UserUpdateInput;
+    },
+  ): Promise<string> {
+    const { where, data } = params;
+
+    try {
+      
+      if (typeof data.password === 'string') {
+        const saltOrRounds = 10;
+        const hashPassword = await bcrypt.hash(data.password, saltOrRounds);
+        data.password = hashPassword;
+      }
+
+      const user = await this.prisma.user.update({
+        data,
+        where,
+      });
+
+      if (!user) {
+        return null;
+      }
+
+      return 'Usuário atualizado com sucesso.';
+    } catch (error) {
+      throw new Error(`Erro ao atualizar usuário: ${error.message}`);
+    }
+  }
+
+  async deleteUserById(where: Prisma.UserWhereUniqueInput): Promise<string> {
+    try {
+      const existingUser = await this.prisma.user.findUnique({
+        where,
+      });
+
+      if (!existingUser) {
+        throw new Error('Usuário não encontrado.');
+      }
+
+      await this.prisma.user.delete({
+        where,
+      });
+
+      return 'Usuário deletado com sucesso.';
+    } catch (error) {
+      throw new Error(`Erro ao excluir usuário: ${error.message}`);
+    }
+  }
+}
